db8b7d9cbd210423eca93afd094b2009ce5736a8
danishyameen <danishyameennew@gmail.com>
2024-12-17 04:59:04 +0500

    update downloadResume builder app

--- resume/resume.js
+++ resume/resume.js
@@ -124,7 +124,7 @@ downloadResume.addEventListener('click', function () {
     if (q.matches) {
         if (resumeArea) {
             var options = {
-                margin: 1, // PDF margin (in cm)
+                margin: 2, // PDF margin (in cm)
                 filename: 'generated.pdf', // Output file name
                 image: { type: 'jpeg', quality: 0.98 }, // Image quality
                 html2canvas: { scale: 2 }, // Canvas scale (higher value = better quality)
@@ -136,7 +136,7 @@ downloadResume.addEventListener('click', function () {
     if (f.matches) {
         if (resumeArea) {
             var options = {
-                margin: 1, // PDF margin (in cm)
+                margin: 2, // PDF margin (in cm)
                 filename: 'generated.pdf', // Output file name
                 image: { type: 'jpeg', quality: 0.98 }, // Image quality
                 html2canvas: { scale: 2 }, // Canvas scale (higher value = better quality)
